67732e555ccf0a38ef2ebb7e041a60e5fd3113ed
Jake Smith <jake.smith92@gmail.com>
2016-09-04 14:03:07 -0500

    Initial commit for dotfiles after fork

--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,6 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+
+sudo npm install babel-cli -g
+sudo npm install eslint -g
---
+++ php/install.sh
@@ -0,0 +1,5 @@
+composer global require squizlabs/php_codesniffer
+composer global require sebastian/phpcpd
+composer global require phpmd/phpmd
+composer global require phpunit/phpunit
+composer global require psy/psysh
